6760fb588c3ee64fe45eab14c117e5d7cc39b9c2
velchand <velchand@inlen8520023381.corp.capgemini.com>
2022-10-27 18:30:24 +0530

    My first use of git

---
+++ src/Array.java
@@ -0,0 +1,19 @@
+import java.util.Arrays;
+class Array {
+	static void arrayReserse(int[]arr){
+	int i = 0;
+	System.out.print("array reverse elements :"+ arr[i]);
+        	for(int i1=arr.length-1;i1>=0;i1--){
+    			 //System.out.print("array reverse elements :"+ arr[i]);
+		}
+        }
+    
+
+public static void main(String[] args){
+	int[] arr = { 5, 2, 1, 8, 10 };
+	arrayReserse(arr);
+}
+}
+
+    
+
